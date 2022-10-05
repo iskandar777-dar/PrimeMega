e3143a5159bc928338716d706833dcfb1baa4c02
iskandar777-dar <54578330+iskandar777-dar@users.noreply.github.com>
2022-10-05 14:10:27 +0700

    Update bot.py

--- PrimeMega/bot.py
+++ PrimeMega/bot.py
@@ -10,23 +10,9 @@ logging.basicConfig(
 
 app = Client(
     "bot",
-    api_id=env.API_ID,
-    api_hash=env.API_HASH,
-    bot_token=env.BOT_TOKEN,
+    api_id=config.API_ID,
+    api_hash=config.API_HASH,
+    bot_token=config.BOT_TOKEN,
     in_memory=True,
     plugins=dict(root="PrimeMega"),
 )
-
-if __name__ == "__main__":
-    print("Starting the String Generator Bot...")
-    try:
-        app.start()
-    except (ApiIdInvalid, ApiIdPublishedFlood):
-        raise Exception("Your API_ID/API_HASH is not valid.")
-    except AccessTokenInvalid:
-        raise Exception("Your BOT_TOKEN is not valid.")
-    uname = app.get_me().username
-    print(f"@{uname} started successfully !")
-    idle()
-    app.stop()
-    print("Bot stopped. Bye !")
