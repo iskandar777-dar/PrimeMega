f379c486669942cd13a30272b214d63686c09258
iskandar777-dar <54578330+iskandar777-dar@users.noreply.github.com>
2022-10-05 13:00:46 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from PrimeMega import config
 import logging
 from pyrogram import Client, idle
-from pyromod import listen  # type: ignore
+from pyrogram import listen  # type: ignore
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 
 logging.basicConfig(
